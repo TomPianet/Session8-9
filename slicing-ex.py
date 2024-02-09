923326acf66e757df72779a37250ce77d9efd0fe
Tom Pianet <tom.pianet.us@gmail.com>
2024-02-09 11:13:30 +0100

    committ

--- slicing-ex.py
+++ slicing-ex.py
@@ -1,7 +1,8 @@
 text = "0123456789abcdef"
-print(text[5:9])
-print(text[:10])
-print(text[10:])
-print(text[-6:])
-print(text[::3])
-print(text[1::2])
+print(text[5:9])       # print from fifth caracter to ninth caracter
+print(text[:10])       #10 premiers caracteres
+print(text[10:])       # start +10 donc a
+print(text[-6:])       #les 6 derniers ( - )
+print(text[::3])       # every 3
+print(text[1::2])      # start +1 then skip 1 and print 3
+print(text[::-1])      #backward
