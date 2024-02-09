82e22d184ad20a5ba1db975f363d6a833e73830c
Tom Pianet <tom.pianet.us@gmail.com>
2024-02-09 12:10:50 +0100

    committ

--- string-method.py
+++ string-method.py
@@ -16,3 +16,24 @@ print("bababannanana".replace("ana", "XXYYZZ"))
 
 sentence = "Hello, kind-sir; how many ! I be of service today ?"
 print(sentence.replace(",", "").replace("Hello", "Hi"))
+
+#elegant way to do it
+punctuation = ".,;!?-"
+for p in punctuation:
+    sentence = sentence.replace(p, " ")
+print(sentence)
+
+
+print("this is a sentence and i want the words".split(""))
+
+
+text = "Bob goes to school. Bob likes to play tennis."
+print(text.find("Bob"))
+print(text.rfind("Bob", i))
+
+while i < len(text):
+    if i == -1:
+        break
+    print(i)
+i += 1
+
