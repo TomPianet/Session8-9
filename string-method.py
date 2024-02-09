e5f8ebfe2f5958ebb9a3d0d2b076c01c61da0d7b
Tom Pianet <tom.pianet.us@gmail.com>
2024-02-09 11:52:52 +0100

    committ

--- string-method.py
+++ string-method.py
@@ -1,3 +1,12 @@
 text = "abcdefghijklmnop"
 print(dir(text))
 help(text.isupper)
+print(text.isupper())
+print("ABC".isupper())  #is the text all uppercase?
+
+print(text.upper())   #convert the text to uppercase
+
+print(text.upper().isupper())   #is the text all uppercase? YESSSSS
+
+new_text = text.upper()
+print(text, new_text)
