979130bd164d95f829e5188a1c3669a2817cd72a
Tom Pianet <tom.pianet.us@gmail.com>
2024-02-09 10:59:59 +0100

    committ

--- Session8-9.py
+++ Session8-9.py
@@ -5,6 +5,16 @@ for letter in text:
 
 i = 0
 while i < len(text):
-    print(text[len(text)-i-1], end="")
+    print(text[i])
     i += 1
 
+i= len(text) - 1
+while i >= 0:
+    print(text[i])
+    i -= 1
+
+print()
+i = 0
+while i < len(text):
+    print(text[len(text)-i-1], end="")
+    i += 1
